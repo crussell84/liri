134135e6236ad4c459fc24ddd47aa62195e59e76
Chris Russell <christine.russell10@gmail.com>
2018-08-20 18:27:00 -0400

    add omdb search

--- code/liri.js
+++ code/liri.js
@@ -1,1 +1,5 @@
 //main js file, will be the file to run on node & will interact with the API JS files
+var omdb = require("./omdb.js");
+var input = process.argv[2];
+
+omdb.movieSearch(input);
--- code/omdb.js
+++ code/omdb.js
@@ -1,1 +1,26 @@
 //modular file for the OMDB API
+var request = require("request");
+
+var movieSearch = function(input){ 
+    if (input === undefined || input === "") {
+        input = "Mr. Nobody";
+    }
+    var title = encodeURI(input);
+    request(`http://www.omdbapi.com/?t=${title}&y=&plot=short&apikey=trilogy`, function(error, response, body) {
+
+  if (!error && response.statusCode === 200) {
+      var data = JSON.parse(body);
+    console.log(`Movie Title: ${data.Title}`);
+    console.log(`Release Year: ${data.Year}`);
+    console.log(`IMDB Rating: ${data.imdbRating}`);
+    console.log(`Rotten Tomatoes Rating: ${data.Ratings[1].Value}`);
+    console.log(`Produced in: ${data.Country}`);
+    console.log(`Spoken Language(s): ${data.Language}`);
+    console.log(`Plot Summary: ${data.Plot}`);
+    console.log(`Cast: ${data.Actors}`);
+  }
+});}
+
+module.exports = {
+    movieSearch: movieSearch
+};
