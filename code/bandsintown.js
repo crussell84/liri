73dc51351d02a6d4e347530aec5de6631da57dd9
Chris Russell <christine.russell10@gmail.com>
2018-08-20 18:50:47 -0400

    add bands in town search

--- code/bandsintown.js
+++ code/bandsintown.js
@@ -1,1 +1,26 @@
 //modular file for the BandsInTown API
+var request = require("request");
+var moment = require("moment");
+
+var bandSearch = function (input) {
+
+    var artist = encodeURI(input);
+    request(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            var data = JSON.parse(body);
+            data.forEach(function (elem) {
+                console.log(elem.venue.name);
+                console.log(elem.venue.city + ", " + elem.venue.region + ", " + elem.venue.country);
+
+                console.log(moment(elem.datetime).format("MM/DD/YYYY"));
+                console.log('~~~~~~');
+            });
+
+        }
+    });
+}
+
+module.exports = {
+    bandSearch: bandSearch
+};
--- code/liri.js
+++ code/liri.js
@@ -1,5 +1,7 @@
 //main js file, will be the file to run on node & will interact with the API JS files
 var omdb = require("./omdb.js");
+var bands = require("./bandsintown");
 var input = process.argv[2];
 
-omdb.movieSearch(input);
+// omdb.movieSearch(input);
+bands.bandSearch(input);
