66fdbc7e424df86341602eb121f2de6c42f4e547
Chris Russell <christine.russell10@gmail.com>
2018-08-20 21:20:29 -0400

    add spotify search

--- code/liri.js
+++ code/liri.js
@@ -1,7 +1,9 @@
 //main js file, will be the file to run on node & will interact with the API JS files
 var omdb = require("./omdb.js");
 var bands = require("./bandsintown");
+var spotify = require("./spotify.js");
 var input = process.argv[2];
 
 // omdb.movieSearch(input);
-bands.bandSearch(input);
+// bands.bandSearch(input);
+spotify.songSearch(input);
